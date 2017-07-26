5c342d6834e79a4392748e5ed5006743e67d30b2
Kurt Opel <kurt@kurtopel.com>
2017-07-26 13:33:09 -0400

    working app

---
+++ src/app/red/red.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-red',
+  template: `
+    <div class="wrapper"></div>
+  `,
+  styles: ['div.wrapper { width: 500px; height: 500px; background-color: red; }']
+})
+export class RedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
